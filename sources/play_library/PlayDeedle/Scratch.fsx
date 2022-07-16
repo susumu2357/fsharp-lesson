5c0c7cc348af8057b44a0baa92c8a9405101ab0d
susumu2357 <susumu.0112357@gmail.com>
2022-07-16 21:40:24 +0200

    Q4 and 5

--- sources/play_library/PlayDeedle/Scratch.fsx
+++ sources/play_library/PlayDeedle/Scratch.fsx
@@ -27,3 +27,33 @@ let math_df =
     |> Frame.ofRows
 
 math_df.Print()
+
+// 課題4: 場所と学年だけのFrameを作ろう
+let selected_df = df.Columns[["場所"; "学年"]]
+selected_df.Print()
+
+// 課題5: フィルタとプロジェクションを関数にしよう
+type Row = ObjectSeries<string>
+and ConditionFunc = Row -> bool
+and Filter = ConditionFunc ->  Frame<int, string> -> Frame<int, string>
+
+let filter:Filter =
+    fun conditionFunc df -> 
+    df.RowsDense 
+    |> Series.filterValues( conditionFunc )
+    |> Frame.ofRows
+
+let myconditionFunc: ConditionFunc = 
+    fun row -> row.Get("専門")="物理"
+
+let filteredFrame = filter myconditionFunc df
+filteredFrame.Print()
+
+type ColumnsList = string list
+and Project = ColumnsList -> Frame<int, string> -> Frame<int, string>
+
+let project: Project =
+    fun columns df -> df.Columns[columns]
+
+let projectedFrame = project ["専門"; "学年"] df
+projectedFrame.Print()
