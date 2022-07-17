f2071c178dc6e50a3a325475aa4257290c3760c5
susumu2357 <susumu.0112357@gmail.com>
2022-07-17 10:21:20 +0200

    use Environment.GetCommandLineArgs()

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,2 +1,19 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello World!!"
+
+open System
+
+let checkHello argArray =
+    argArray
+    |> List.ofArray
+    |> List.map (fun s -> s="hello")
+    |> List.fold (fun result b -> result || b) false
+
+let arg = Environment.GetCommandLineArgs()
+printfn "%A" arg
+
+let isHello = checkHello arg
+
+if isHello then 
+    printfn "Hello World!!"
+else
+    printfn "I don't know"
