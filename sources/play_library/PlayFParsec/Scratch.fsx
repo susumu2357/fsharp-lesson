b7c8c78842b2da37ea0959d38944c12f309ceca2
susumu2357 <susumu.0112357@gmail.com>
2022-07-18 22:32:17 +0200

    Q10 revised

--- sources/play_library/PlayFParsec/Scratch.fsx
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -16,3 +16,19 @@ run pColumn " 　[ 　学年  　] 　"
 // カラム名の間にあるスペースも無視
 run pColumn "[学 年]"
 run pColumn "[学　年]"
+
+let ws2 = manyChars (anyOf [' '; '　'])
+run ws2 " 　abc"
+
+let str_ws2 s = pstring s .>> ws2
+
+let notBracket = satisfy (fun c -> c <> '[' && c <> ']')
+let tmpParser = (pstring "[") >>. (manyChars notBracket) .>> (str_ws2 "]")
+let pColumn2 = ws2 >>. tmpParser
+
+// カッコの前後のスペースは無視
+run pColumn2 " 　[名前]  　"
+
+// カッコの内側のスペースはそのままパース
+run pColumn2 " 　[名 前]  　"
+run pColumn2 " 　[名前　学年]  　"
