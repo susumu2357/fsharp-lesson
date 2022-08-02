39cfd0df01077dbd5d4787f230d7abab2b996110
susumu2357 <susumu.0112357@gmail.com>
2022-08-02 10:45:52 +0200

    Q-evalProjectExpression

--- sources/ToyRel/Scratch.fsx
+++ sources/ToyRel/Scratch.fsx
@@ -27,8 +27,10 @@ let pColumnList = sepBy pColumn (str_ws ",") |>> ColumnList
 run pColumnList "a, b, [123_abc]"
 
 type Expression =
-    | Identifier of string
-    | ProjectExpression of Expression * ColumnList
+    | Identifier of Identifier: string
+    | ProjectExpression of ProjectExpression
+
+and ProjectExpression = Expression * ColumnList
 
 let pExpression, pExpressionRef = createParserForwardedToRef ()
 
@@ -53,3 +55,51 @@ run pProjectExpression "project (project (シラバス) 専門, 学年, 場所) 
 
 // ProjectExpression (ProjectExpression (Identifier "シラバス", ColumnList ["123_専門"]), ColumnList ["専門"; "学年"])
 run pProjectExpression "project (project (シラバス) [123_専門]) 専門, 学年"
+
+// 第一回と同じ感じのprojectでdfを返す所まで実装してみよう
+#r "nuget:Deedle"
+open Deedle
+
+type EvalExpression = Expression -> Frame<int, string>
+and EvalProjectExpression = ProjectExpression -> Frame<int, string>
+
+let rec evalExpression: EvalExpression =
+    fun exp ->
+        match exp with
+        | Identifier identifier ->
+            let filepath =
+                ".\\sources\\ToyRel\\database\\master\\"
+                + identifier
+                + ".csv"
+
+            Frame.ReadCsv filepath
+        | ProjectExpression projectExpression -> evalProjectExpression projectExpression
+
+and evalProjectExpression: EvalProjectExpression =
+    fun projectExp ->
+        let (exp, columnList) = projectExp
+        let df = evalExpression exp
+        let (ColumnList columns) = columnList
+        df.Columns[columns]
+
+// 以下、動作確認
+
+let paserResult parser str =
+    match run parser str with
+    | CharParsers.Success (result, _, _) ->
+        match result with
+        | ProjectExpression projectExpression -> projectExpression
+        | Identifier _ -> failwithf "Try to test on ProjectExpression"
+    | CharParsers.Failure (errorMsg, _, _) -> failwithf "Failure: %s" errorMsg
+
+let test1ProjectExpression =
+    paserResult pProjectExpression "project (シラバス) 専門, 学年, 場所"
+
+let test1Df = evalProjectExpression test1ProjectExpression
+test1Df.Print()
+
+let test2ProjectExpression =
+    paserResult pProjectExpression "project (project (シラバス) 専門, 学年, 場所) 専門, 学年"
+
+let test2Df = evalProjectExpression test2ProjectExpression
+test2Df.Print()
