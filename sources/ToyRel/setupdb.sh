e6443a089ac8afd7fe7703f6bda913c643d93e01
susumu2357 <susumu.0112357@gmail.com>
2022-08-09 10:09:36 +0200

    Q12 wikipedia DB

--- sources/ToyRel/setupdb.sh
+++ sources/ToyRel/setupdb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm -rf database/master database/library database/glossary
-mkdir -p database/master database/library database/glossary
-cp ../data/シラバス.csv ../data/wikipedia/*.csv  database/master
+rm -rf database/master database/wikipedia database/library database/glossary
+mkdir -p database/master database/wikipedia database/library database/glossary
+cp ../data/シラバス.csv ../data/tandp/lc.csv  database/master
 cp ../data/tandp/book.csv ../data/tandp/index.csv ../data/tandp/subject.csv ../data/tandp/auction.csv database/library
 cp ../data/tandp/goods.csv ../data/tandp/delivery.csv ../data/tandp/stock.csv database/glossary
+cp ../data/wikipedia/*.csv database/wikipedia
