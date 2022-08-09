83971bbfc73120d2f2a134bce210f7aee16696b1
susumu2357 <susumu.0112357@gmail.com>
2022-08-09 09:51:39 +0200

    Q12 library and glossary DB

--- sources/ToyRel/setupdb.sh
+++ sources/ToyRel/setupdb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm -rf database/master database/tandp
-mkdir -p database/master database/tandp
+rm -rf database/master database/library database/glossary
+mkdir -p database/master database/library database/glossary
 cp ../data/シラバス.csv ../data/wikipedia/*.csv  database/master
-cp ../data/tandp/*.csv database/tandp
+cp ../data/tandp/book.csv ../data/tandp/index.csv ../data/tandp/subject.csv ../data/tandp/auction.csv database/library
+cp ../data/tandp/goods.csv ../data/tandp/delivery.csv ../data/tandp/stock.csv database/glossary
