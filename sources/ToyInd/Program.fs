8f42a34e175e529e082ad2c93835bc4d5a96d108
susumu2357 <susumu.0112357@gmail.com>
2022-09-19 23:27:04 +0200

    wip simple word search

---
+++ sources/ToyInd/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+
+let path = DirectoryInfo("C:\\Users\\susum\\Documents\\fsharp-lesson\\sources\\ToyInd\\test_target")
+
+let files = path.EnumerateFiles()
+
+
+files
+|> Seq.cast<string>
+|> Seq.map File.ReadLines 
+|> printfn "%A"
