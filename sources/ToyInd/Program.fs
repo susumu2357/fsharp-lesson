49836e4343cd9ce32be0629a7083a543067cd0cd
susumu2357 <susumu.0112357@gmail.com>
2022-09-20 22:46:33 +0200

    Q02

--- sources/ToyInd/Program.fs
+++ sources/ToyInd/Program.fs
@@ -1,11 +1,34 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
-let path = DirectoryInfo("C:\\Users\\susum\\Documents\\fsharp-lesson\\sources\\ToyInd\\test_target")
+let excludingExtensions = [".png"; ".jpg"; ".jpeg"] |> Collections.Generic.HashSet
 
-let files = path.EnumerateFiles()
+/// <summary>Search the targetWord in the files located in the targetDir.</summary>
+/// <returns>A sequence of "filepath: line" where the line contains the targetWord.</returns>
+let searchWord targetDir targetWord =
+    let files = Directory.EnumerateFiles(targetDir)
 
+    // Keep lines which contain the targetWord for each file.
+    let filteredLines =
+        files
+        |> Seq.filter ( fun file -> not (excludingExtensions.Contains (FileInfo(file).Extension)))
+        |> Seq.map File.ReadLines
+        |> Seq.map (fun lines -> lines |> Seq.cast<string>)
+        |> Seq.map (fun lines -> 
+            lines |>
+                Seq.filter (
+                    fun line -> line.Contains (targetWord:string)
+        ))
+    
+    // Combine file names and filtered lines.
+    (files, filteredLines)
+    ||> Seq.map2 (fun file lines ->
+        lines
+        |> Seq.map (fun line -> file + ": " + line)
+     )
+    |> Seq.collect id
 
-files
-|> Seq.cast<string>
-|> Seq.map File.ReadLines 
-|> printfn "%A"
+[<EntryPoint>]
+let main args =
+    printfn "%A" (searchWord args[0] args[1])
+    0
---
+++ sources/ToyInd/Scratch.fsx
@@ -0,0 +1,36 @@
+open System
+open System.IO
+
+Environment.CurrentDirectory
+// Environment.CurrentDirectory <- @"C:\Users\susum\OneDrive\Documents\fsharp\fsharp-lesson\sources\ToyInd"
+Environment.CurrentDirectory <- @"C:\Users\susum\Documents\fsharp-lesson\sources\ToyInd"
+
+
+let excludingExtensions = [".png"; ".jpg"; ".jpeg"] |> Collections.Generic.HashSet
+
+/// <summary>Search the targetWord in the files located in the targetDir.</summary>
+/// <returns>A sequence of "filepath: line" where the line contains the targetWord.</returns>
+let searchWord targetDir targetWord =
+    let files = Directory.EnumerateFiles(targetDir)
+
+    // Keep lines which contain the targetWord for each file.
+    let filteredLines =
+        files
+        |> Seq.filter ( fun file -> not (excludingExtensions.Contains (FileInfo(file).Extension)))
+        |> Seq.map File.ReadLines
+        |> Seq.map (fun lines -> lines |> Seq.cast<string>)
+        |> Seq.map (fun lines -> 
+            lines |>
+                Seq.filter (
+                    fun line -> line.Contains (targetWord:string)
+        ))
+    
+    // Combine file names and filtered lines.
+    (files, filteredLines)
+    ||> Seq.map2 (fun file lines ->
+        lines
+        |> Seq.map (fun line -> file + ": " + line)
+     )
+    |> Seq.collect id
+
+searchWord "test_target" "123"
