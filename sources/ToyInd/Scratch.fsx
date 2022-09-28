18e684225de2d9d2431ee0fd8666c4e949585e75
susumu <susumu.0112357@gmail.com>
2022-09-28 23:04:29 +0200

    measure time and memory usage with FParsec

--- sources/ToyInd/Scratch.fsx
+++ sources/ToyInd/Scratch.fsx
@@ -5,6 +5,55 @@ open System.IO
 open SimpleSearch
 
 // Environment.CurrentDirectory <- @"C:\Users\susum\OneDrive\Documents\fsharp\fsharp-lesson\sources\ToyInd"
-Environment.CurrentDirectory <- @"C:\Users\susum\Documents\fsharp-lesson\sources\ToyInd"
+// Environment.CurrentDirectory <- @"C:\Users\susum\Documents\fsharp-lesson\sources\ToyInd"
 
 searchWordUnderDir "pipe3" "test_target"
+
+#load "executeProcess.fs"
+open executeProcess
+
+type Command =
+    | Ag
+    | Grep
+    | FsharpSimple // SimpleSearch
+
+// Prepare labels
+let createLabels datasetName =
+    [Ag; Grep; FsharpSimple]
+    |> List.map (fun case ->
+        match case with
+        | Ag -> datasetName + "_ag"
+        | Grep -> datasetName + "_grep"
+        | FsharpSimple -> datasetName + "_fsharpSimple"
+    )
+let labels = createLabels "fparsec"
+
+
+let createCommand targetWord targetDir commandType =
+    let addArguments c =
+        c + " " + targetWord + " " + targetDir
+        
+    match commandType with
+    | Ag -> addArguments "ag"
+    | Grep -> addArguments "grep -R"
+    | FsharpSimple -> addArguments "dotnet run -c Release"
+
+// Bake in targetWord and targetDir
+let command = createCommand "pipe3" "test_target/fparsec"
+
+// Measure time and memory usage for each command.
+let results =
+    [Ag; Grep; FsharpSimple]
+    |> List.map command
+    |> List.map (fun commandString -> "-f \"%e,%M\" " + commandString)
+    |> List.map (executeProcess "/usr/bin/time")
+    |> List.map (fun result -> result.StdErr)
+
+// Add the header
+let resultsWithLabels =
+    ["label,time(sec),memory(kB)"] @ (
+        (labels, results) 
+        ||> List.map2 (fun label results -> label + "," + results)
+        )
+
+File.WriteAllLines("benchmarkResults.csv", resultsWithLabels)
---
+++ sources/ToyInd/executeProcess.fs
@@ -0,0 +1,30 @@
+// Copied from
+// https://isthisit.nz/posts/2021/execute-a-shell-process-in-fsharp/
+// which is based on
+// https://stackoverflow.com/questions/3065409/starting-a-process-synchronously-and-streaming-the-output/3065876#3065876
+
+module executeProcess
+
+open System
+
+type ProcessResult = { 
+    ExitCode : int; 
+    StdOut : string; 
+    StdErr : string 
+}
+
+let executeProcess (processName: string) (processArgs: string) =
+    let psi = new Diagnostics.ProcessStartInfo(processName, processArgs) 
+    psi.UseShellExecute <- false
+    psi.RedirectStandardOutput <- true
+    psi.RedirectStandardError <- true
+    psi.CreateNoWindow <- true        
+    let proc = Diagnostics.Process.Start(psi) 
+    let output = new Text.StringBuilder()
+    let error = new Text.StringBuilder()
+    proc.OutputDataReceived.Add(fun args -> output.Append(args.Data) |> ignore)
+    proc.ErrorDataReceived.Add(fun args -> error.Append(args.Data) |> ignore)
+    proc.BeginErrorReadLine()
+    proc.BeginOutputReadLine()
+    proc.WaitForExit()
+    { ExitCode = proc.ExitCode; StdOut = output.ToString(); StdErr = error.ToString() }
