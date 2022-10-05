14264b67388588d200c827c7888e44b1f5ac3f82
susumu <susumu.0112357@gmail.com>
2022-10-05 22:26:05 +0200

    measure file index stats

--- sources/ToyInd/Scratch.fsx
+++ sources/ToyInd/Scratch.fsx
@@ -41,14 +41,112 @@ deleteFsprojUnderDir "test_target/fsharp"
 #load "FileIndex.fs"
 open FileIndex
 
+// Measure FParsec
+// Create path_list
+let mutable stopWatch = Diagnostics.Stopwatch.StartNew()
 FileIndex.createFileIndex "test_target/fparsec"
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Load path_list and look up once
+stopWatch <- Diagnostics.Stopwatch.StartNew()
 let fileIndexFparsec = FileIndex.loadFileIndex "test_index/fparsec"
 FileIndex.lookupFileId fileIndexFparsec "test_target/fparsec/Samples/FSharpParsingSample/FParsecVersion/parser.fs" // 166
+printfn "%d" (Diagnostics.Process.GetCurrentProcess().WorkingSet64)
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
 
+// Load path_list and look up 1000 times
+let shuffle (r : Random) xs = xs |> Seq.sortBy (fun _ -> r.Next())
+let randomFparsecFiles = 
+    shuffle (Random ()) (File.ReadAllLines("test_index/fparsec/path_list.txt"))
+    |> Seq.take 1000
+
+stopWatch <- Diagnostics.Stopwatch.StartNew()
+let lookupFparsec = FileIndex.lookupFileId (FileIndex.loadFileIndex "test_index/fparsec")
+randomFparsecFiles
+|> Seq.map lookupFparsec
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Measure Fsharp
+// Create path_list
+stopWatch <- Diagnostics.Stopwatch.StartNew()
 FileIndex.createFileIndex "test_target/fsharp"
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Load path_list and look up once
+stopWatch <- Diagnostics.Stopwatch.StartNew()
 let fileIndexFsharp = FileIndex.loadFileIndex "test_index/fsharp"
 FileIndex.lookupFileId fileIndexFsharp "test_target/fsharp/tests/fsharp/core/unicode/kanji-unicode-utf8-withsig-codepage-65001.fs" // 3559
+printfn "%d" (Diagnostics.Process.GetCurrentProcess().WorkingSet64)
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
 
+// Load path_list and look up 1000 times
+let randomFsharpFiles = 
+    shuffle (Random ()) (File.ReadAllLines("test_index/fsharp/path_list.txt"))
+    |> Seq.take 1000
+
+stopWatch <- Diagnostics.Stopwatch.StartNew()
+let lookupFsharp = FileIndex.lookupFileId (FileIndex.loadFileIndex "test_index/fsharp")
+randomFsharpFiles
+|> Seq.map lookupFsharp
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+
+// Measure MongoDB
+// Create path_list
+stopWatch <- Diagnostics.Stopwatch.StartNew()
+FileIndex.createFileIndex "test_target/mongo"
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Load path_list and look up once
+stopWatch <- Diagnostics.Stopwatch.StartNew()
+let fileIndexMongo = FileIndex.loadFileIndex "test_index/mongo"
+FileIndex.lookupFileId fileIndexMongo "test_target/mongo/src/third_party/IntelRDFPMathLib20U1/LIBRARY/float128/sizeof.c" // 15820
+printfn "%d" (Diagnostics.Process.GetCurrentProcess().WorkingSet64)
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Load path_list and look up 1000 times
+let randomMongoFiles = 
+    shuffle (Random ()) (File.ReadAllLines("test_index/mongo/path_list.txt"))
+    |> Seq.take 1000
+
+stopWatch <- Diagnostics.Stopwatch.StartNew()
+let lookupMongo = FileIndex.lookupFileId (FileIndex.loadFileIndex "test_index/mongo")
+randomMongoFiles
+|> Seq.map lookupMongo
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Measure Elsevier
+// Create path_list
+stopWatch <- Diagnostics.Stopwatch.StartNew()
 FileIndex.createFileIndex "test_target/elsevier"
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Load path_list and look up once
+stopWatch <- Diagnostics.Stopwatch.StartNew()
 let fileIndexElsevier = FileIndex.loadFileIndex "test_index/elsevier"
 FileIndex.lookupFileId fileIndexElsevier "test_target/elsevier/json/S0378377418304001.json" // 36437
+printfn "%d" (Diagnostics.Process.GetCurrentProcess().WorkingSet64)
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+
+// Load path_list and look up 1000 times
+let randomElsevierFiles = 
+    shuffle (Random ()) (File.ReadAllLines("test_index/elsevier/path_list.txt"))
+    |> Seq.take 1000
+
+stopWatch <- Diagnostics.Stopwatch.StartNew()
+let lookupElsevier = FileIndex.lookupFileId (FileIndex.loadFileIndex "test_index/elsevier")
+randomElsevierFiles
+|> Seq.map lookupElsevier
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
---
+++ sources/ToyInd/fileIndex.md
@@ -0,0 +1,14 @@
+<!-- 
+data,create_path_list(msec),size_path_list(Bytes),loading_looking_up_1_path_list(msec),loading_looking_up_1_path_list(Bytes),loading_looking_up_1000_path_list(msec)
+fparsec,1.966,12K,9.281,183M,1.228
+fsharp,45.03,744K,18.09,193M,28.82
+mongo,108.94,2.8M,198.93,213M,152.7
+elsevier,61.67,1.9M,190.6,236M,213.1 
+-->
+
+|data    |create_path_list(msec)|size_path_list(Bytes)|loading_looking_up_1_path_list(msec)|loading_looking_up_1_path_list(Bytes)|loading_looking_up_1000_path_list(msec)|
+|--------|----------------------|---------------------|------------------------------------|-------------------------------------|---------------------------------------|
+|fparsec |1.966                 |12K                  |9.281                               |183M                                 |1.228                                  |
+|fsharp  |45.03                 |744K                 |18.09                               |193M                                 |28.82                                  |
+|mongo   |108.94                |2.8M                 |198.93                              |213M                                 |152.7                                  |
+|elsevier|61.67                 |1.9M                 |190.6                               |236M                                 |213.1                                  |
